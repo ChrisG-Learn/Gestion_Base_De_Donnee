847414f81165a89c4fb6230491af02ac832eb170
synergeur <79270978+synergeur@users.noreply.github.com>
2021-03-09 09:16:20 -0500

    Form

--- WFA_Coktail_Management/Config.cs
+++ WFA_Coktail_Management/Config.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace WFA_Coktail_Management
 {
     class Config
     {
+       protected SqlConnection GetConnection()
+        {
+            SqlConnection Cnn = new SqlConnection();
+            Cnn.ConnectionString = "Data Source=LAPTOP-M4VGGB05;Initial Catalog=SD-CG_Travail1_DB;Integrated Security=True";
+            return Cnn;
+        }
     }
 }
