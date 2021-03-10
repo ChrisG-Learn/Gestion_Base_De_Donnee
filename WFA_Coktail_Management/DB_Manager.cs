3aa252b40a2501d2156772cf869ad43630186eca
Christopher <regisgoupil67@gmail.com>
2021-03-10 13:37:42 -0500

    Add function to DB_Manager.s

--- WFA_Coktail_Management/DB_Manager.cs
+++ WFA_Coktail_Management/DB_Manager.cs
@@ -27,7 +27,50 @@ namespace WFA_Coktail_Management
                 sqlCommand.Parameters["@Level"].Value = difficulty;
                 sqlCommand.Parameters["@Quotation"].Value = quotation;
                 sqlCommand.Parameters["@Category"].Value = category;
+                connection.Open();
+
+                sqlCommand.ExecuteNonQuery();
             }
         }
+
+        public void create_Category(string categoryName)
+        {
+
+        }
+
+        public SqlDataReader list_Cocktail_Category(string choice="Cocktail")
+        {
+            using (SqlConnection sqlConnection = getConnection())
+            {
+                SqlDataReader sqlDatareader;
+                string sql;
+                if (choice == "Cocktail")
+                    sql = "SELECT cocktail_Id, cocktail_name, difficulty_level, quotation, category_Id FROM tbl_cocktail ORDER BY cocktail_name"; 
+                else
+                    sql = "SELECT category_Id, category FROM tbl_category ORDER BY category"; 
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                sqlConnection.Open();
+                sqlDatareader = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                return sqlDatareader;
+            }
+        }
+
+        public SqlDataReader get_Information(int ID, string choice= "Cocktail")
+        {
+            using (SqlConnection sqlConnection = getConnection())
+            {
+                string sql;
+                if (choice == "Cocktail")
+                    sql = "SELECT cocktail_name, difficulty_level, quotation, category_Id, cocktail_Id FROM  tbl_cocktail WHERE cocktail_ID = " + ID;
+                else
+                    sql = "SELECT category_Id, category FROM tbl_category WHERE category_Id = " + ID;
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                return sqlDataReader;
+            }
+        }
+
+
     }
 }
