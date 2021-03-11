033125faa4aeb4f78434e99da56df7adffc0d4d7
Christopher <regisgoupil67@gmail.com>
2021-03-11 09:32:51 -0500

    [FINISH]  UPDATE DB_Manager.cs

--- WFA_Coktail_Management/DB_Manager.cs
+++ WFA_Coktail_Management/DB_Manager.cs
@@ -63,7 +63,7 @@ namespace WFA_Coktail_Management
 
         }
 
-        public SqlDataReader get_Information(int ID, string choice= "Cocktail")
+        public SqlDataReader get_Information(int ID, string choice)
         {
             using (SqlConnection sqlConnection = getConnection())
             {
@@ -79,6 +79,43 @@ namespace WFA_Coktail_Management
             }
         }
 
+        public void modify_Cocktail(string name, string difficulty, int quotation, int category, int ID)
+        {
+            using (SqlConnection sqlConnection = getConnection())
+            {
+                string sql = "modifyCocktail";
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.Add("@Name", SqlDbType.VarChar, 100);
+                sqlCommand.Parameters.Add("@Level", SqlDbType.VarChar, 50);
+                sqlCommand.Parameters.Add("@Quotation", SqlDbType.Int);
+                sqlCommand.Parameters.Add("@Category", SqlDbType.Int);
+                sqlCommand.Parameters.Add("@ID", SqlDbType.Int);
+                sqlCommand.Parameters["@Name"].Value = name;
+                sqlCommand.Parameters["@Level"].Value = difficulty;
+                sqlCommand.Parameters["@Quotation"].Value = quotation;
+                sqlCommand.Parameters["@Category"].Value = category;
+                sqlCommand.Parameters["@ID"].Value = ID;
 
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+
+        public void modify_Category(string categoryName, int ID)
+        {
+            using (SqlConnection sqlConnection = getConnection())
+            {
+                string sql = "modifyCategory";
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.Add("@Category", SqlDbType.VarChar, 50);
+                sqlCommand.Parameters.Add("@ID", SqlDbType.Int);
+                sqlCommand.Parameters["@Category"].Value = categoryName;
+                sqlCommand.Parameters["@ID"].Value = ID;
+                sqlConnection.Open();
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
     }
 }
