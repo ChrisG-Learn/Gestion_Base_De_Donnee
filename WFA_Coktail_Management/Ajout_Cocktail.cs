6e5159df6387aae5e1573ecb9460a75adc39965e
synergeur <79270978+synergeur@users.noreply.github.com>
2021-03-17 14:35:43 -0400

    ajoutCock

--- WFA_Coktail_Management/Ajout_Cocktail.cs
+++ WFA_Coktail_Management/Ajout_Cocktail.cs
@@ -12,6 +12,7 @@ namespace WFA_Coktail_Management
 {
     public partial class Ajout_Cocktail : Form
     {
+        DB_Manager dB_Manager = new DB_Manager();
         public Ajout_Cocktail()
         {
             InitializeComponent();
@@ -25,39 +26,40 @@ namespace WFA_Coktail_Management
             {
                 cmbQuotation.Items.Add(i);
             }
+            cmbQuotation.DropDownStyle = ComboBoxStyle.DropDownList;
         }
 
-        private void categoryChecked()
+        private string categoryChecked()
         {
-            //if ()
-            //{
-            //    rdbMinuteCockatailAdd.Checked = true;
-            //}
-            //else
-            //{
-            //    rdbMinuteCockatailAdd.Checked = false;
-            //}
-            //if ()
-            //{
-            //    rdbStunningAdd.Checked = true;
-            //}
-            //else
-            //{
-            //    rdbStunningAdd.Checked = false;
-            //}
-            //if ()
-            //{
-            //    rdbForCuriousAdd.Checked = true;
-            //}
-            //else
-            //{
-            //    rdbForCuriousAdd.Checked = false;
-            //}
+            if (rdbMinuteCockatailAdd.Checked == true && rdbStunningAdd.Checked == false && rdbForCuriousAdd.Checked == false)
+            {
+                return "minute cocktail";
+            }
+            else
+            {
+                return "";
+            }
+            if (rdbStunningAdd.Checked == true && rdbForCuriousAdd.Checked == false && rdbMinuteCockatailAdd.Checked == false)
+            {
+                return "stunning";
+            }
+            else
+            {
+                return "";
+            }
+            if (rdbForCuriousAdd.Checked = true && rdbStunningAdd.Checked == false && rdbMinuteCockatailAdd.Checked == false)
+            {
+                return "for curious";
+            }
+            else
+            {
+                return "";
+            }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            dB_Manager.create_Cocktail(txtName.Text, categoryChecked(), int.Parse(cmbQuotation.SelectedItem.ToString()), int.Parse(cmbCategory.SelectedIndex.ToString()));
         }
 
         private void label1_Click(object sender, EventArgs e)
