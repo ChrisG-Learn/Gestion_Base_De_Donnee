7364cdc2e800d5da8cfc76479ea089fbfe24e9cc
synergeur <79270978+synergeur@users.noreply.github.com>
2021-04-14 14:25:04 -0400

    form

---
+++ WFA_Coktail_Management/Les_catégorie_et_leur_cocktails.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace WFA_Coktail_Management
+{
+    partial class Les_catégorie_et_leur_cocktails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCategorie = new System.Windows.Forms.Label();
+            this.lblCocktails = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCategorie
+            // 
+            this.lblCategorie.AutoSize = true;
+            this.lblCategorie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.lblCategorie.Location = new System.Drawing.Point(30, 22);
+            this.lblCategorie.Name = "lblCategorie";
+            this.lblCategorie.Size = new System.Drawing.Size(100, 18);
+            this.lblCategorie.TabIndex = 0;
+            this.lblCategorie.Text = "Les Catégorie";
+            // 
+            // lblCocktails
+            // 
+            this.lblCocktails.AutoSize = true;
+            this.lblCocktails.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCocktails.Location = new System.Drawing.Point(30, 249);
+            this.lblCocktails.Name = "lblCocktails";
+            this.lblCocktails.Size = new System.Drawing.Size(108, 18);
+            this.lblCocktails.TabIndex = 1;
+            this.lblCocktails.Text = "Leurs cocktails";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 43);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(442, 203);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(33, 270);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(442, 203);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // Les_catégorie_et_leur_cocktails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 483);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblCocktails);
+            this.Controls.Add(this.lblCategorie);
+            this.Name = "Les_catégorie_et_leur_cocktails";
+            this.Text = "Les_catégorie_et_leur_cocktails";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCategorie;
+        private System.Windows.Forms.Label lblCocktails;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
