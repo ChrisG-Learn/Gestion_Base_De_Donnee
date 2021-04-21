fef256fddf70fa58b0d7943445c9be12a7d6b5cc
synergeur <79270978+synergeur@users.noreply.github.com>
2021-04-21 13:20:58 -0400

    form2

--- WFA_Coktail_Management/Form/IngredientPourCocktails.cs
+++ WFA_Coktail_Management/Form/IngredientPourCocktails.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WFA_Coktail_Management
 {
@@ -26,9 +27,29 @@ namespace WFA_Coktail_Management
             utility.fill_dgv_cocktail(dgv_cocktailParIngredient);
         }
 
+        private void fillDgv_Ingredient_reserched(int id)
+        {
+            DB_Manager db_Manager = new DB_Manager();
+            BindingSource Bs = new BindingSource();
+
+            using (SqlDataReader Dr = db_Manager.
+            {
+                Bs.DataSource = Dr;
+                if (Bs.Count > 1)
+                {
+                    dgv_cocktailParIngredient.DataSource = Bs;
+                }
+                else
+                    MessageBox.Show("No cocktails whit this ingredient");
+            }
+            dgv_cocktailParIngredient.RowHeadersVisible = false;
+            dgv_cocktailParIngredient.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
         private void cmb_ingredients_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            int Id = Convert.ToInt32(cmb_ingredients.SelectedValue);
+            fillDgv_Ingredient_reserched(Id);
         }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
